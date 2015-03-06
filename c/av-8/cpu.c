ad589a8f16cc46041d8eaba7e096c2183a02a408
ar <ar@komputer>
2015-03-05 19:51:09 -0700

    Adding local changes

--- c/av-8/cpu.c
+++ c/av-8/cpu.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "instr.h"
 
 #define ROMNAME "rom.bin"
 
@@ -9,83 +8,139 @@
 #define FLASHEND 0xFFFF
 #define FLASHSIZE FLASHEND 
 
-/* Status Register */
-struct SR 
+struct CHIP 
 {
-  unsigned int C:1; /* Carry flag */
-  unsigned int Z:1; /* Zero flag */ 
-  unsigned int N:1; /* Negative flag */
-  unsigned int V:1; /* Twos complement overflow indicator */
-  unsigned int S:1; /* N^V (For signed tests) */
-  unsigned int H:1; /* Half carry flag */
-  unsigned int T:1; /* Transfer bit (BLD/BST) */
-  unsigned int I:1; /* Global interrupt enable|disable */
+
+  struct STATUS_REGISTER 
+  {
+    unsigned int C:1; /* Carry flag */
+    unsigned int Z:1; /* Zero flag */ 
+    unsigned int N:1; /* Negative flag */
+    unsigned int V:1; /* Twos complement overflow indicator */
+    unsigned int S:1; /* N^V (For signed tests) */
+    unsigned int H:1; /* Half carry flag */
+    unsigned int T:1; /* Transfer bit (BLD/BST) */
+    unsigned int I:1; /* Global interrupt enable|disable */
+  } SR;
+
+  /* 32 General Purpose registers */
+  unsigned short R[32];  
+
+  /* Flash - Program Memory */
+  unsigned short FLASH[0xFFFF];
+
+  /* Data Memory */
+  unsigned short IO[64];       /* 64 Locations */
+  unsigned char  DATA[0xFFFF]; /* 64k          */
+
+  /* RO Registers */
+  unsigned short SP;
+  unsigned short PC;
+
 };
 
-struct SR SREG;
+void
+DEC (struct CHIP *C, unsigned short I) 
+{
 
-/* Instruction macros */
-#define DEC(I) \
-   (R[I] & 0x8000) ? (SREG.C=1) : (0) ; \
-    R[I]--; \
-   (R[I] == 0 ) ? (SREG.Z=1) : (SREG.Z=0) ; \
-   (R[I] & 0x8000) ? (SREG.N=1) : (SREG.N=0) ; \
-   (SREG.S = SREG.N ^ SREG.V) ; \
-   PC++; \
-
-#define NOP() PC++
-
-/* Flash - Program Memory */
-unsigned short FLASH[0xFFFF];
-
-/* Data Memory */
-unsigned short R[32];        /* 32 Registers */
-unsigned short IO[64];       /* 64 Locations */
-unsigned char  DATA[0xFFFF]; /* 64k          */
-
-/* RO Registers */
-unsigned short SP;
-unsigned short PC;
-
-void loadRom();
-void printRegs();
-void printFlags();
-
-int main() {
-
-    register unsigned int i;
-
-    /* Initialize all registers to 0 */
-    SP = 0;
-    PC = 0;
-
-    /* Load the ROM */
-    loadRom();
-
-    /* Start the CPU */
-    do {
-
-        if( FLASH[PC] == 0 )
-            NOP();
-
-        if( (FLASH[PC] & 0xFE0F) == 0x940A ) /* 1001010-----1010 */
-            DEC( FLASH[PC] & 0x1F0 );
+   C->R[I] & 0x8000 ? (C->SR.V=1) : (C->SR.V=0); 
+   C->R[I] & 0x8000 ? (C->SR.V=1) : (C->SR.V=0); 
+   C->R[I]--; 
+   C->R[I] == 0 ? (C->SR.Z=1) : (C->SR.Z=0); 
+   C->R[I] & 0x8000 ? (C->SR.N=1) : (C->SR.N=0); 
+   C->SR.S = C->SR.N ^ C->SR.V; 
+   C->PC++;
  
-        if(DEBUG) {
-            printf("Executed instruction: %x\n", FLASH[PC]);
-            printRegs();
-            printFlags();
-        }
- 
-    } while ( PC < FLASHEND ); 
+}
 
-    printf("CPU Has Executed Flash Contents.\n");  
+void 
+NOP (struct CHIP *C) 
+{
 
-    return 0;
+  C->PC++;
 
 }
 
-void loadRom() {
+void 
+ADD (struct CHIP *C, unsigned short dst, unsigned short src) 
+{
+
+  C->R[dst] = C->R[src] + C->R[dst]; 
+  C->R[dst] & 0x8000 ? (C->SR.N=1) : (C->SR.N=0); 
+  C->R[dst] == 0 ? (C->SR.Z=1) : (C->SR.Z=0);
+  C->SR.S = C->SR.N ^ C->SR.V;
+  C->PC++; 
+
+}
+
+void 
+ADC (struct CHIP *C, unsigned short dst, unsigned short src)
+{
+
+  unsigned short carry  = C->R[dst] & C->R[src];
+  unsigned short result = C->R[dst] ^ C->R[src];
+  unsigned short sc = 0;
+
+  C->SR.H = (carry & 0x80) ? 1 : 0;
+
+  while( carry != 0) {
+    sc = carry << 1;
+    carry = result & sc;
+    result ^= sc;
+  }
+
+  C->R[dst] = result;
+
+}
+
+void flashRom(struct CHIP *);
+void printRegs(struct CHIP *);
+void printFlags(struct CHIP *);
+
+int 
+main () 
+{
+
+  struct CHIP chip;
+  struct CHIP *C;
+  register unsigned int i;
+
+  C = &chip;
+
+  /* Initialize all registers to 0 */
+  C->SP = 0;
+  C->PC = 0;
+
+  /* Load the ROM onto the chip */
+  flashRom(C);
+
+  /* Start the CPU */
+  do {
+
+    if( C->FLASH[C->PC] == 0 )
+      NOP(C);
+
+    if( (C->FLASH[C->PC] & 0xFE0F) == 0x940A ) /* 1001010-----1010 */
+      DEC( C, (C->FLASH[C->PC] & 0x1F0) );
+
+//  if( )
+//   ADD();
+ 
+    if(DEBUG) {
+      printf("Executed instruction: %x\n", C->FLASH[C->PC]);
+      printRegs(C);
+      printFlags(C);
+    }
+ 
+  } while ( C->PC < FLASHEND ); 
+
+  printf("CPU Has Executed Flash Contents.\n");  
+
+  return 0;
+
+}
+
+void flashRom(struct CHIP *C) {
 
     FILE *rom_file;
     unsigned int len;
@@ -94,7 +149,7 @@ void loadRom() {
 
     if (rom_file) {
       
-        len = fread(FLASH, 2, FLASHSIZE, rom_file);
+        len = fread(C->FLASH, 2, FLASHSIZE, rom_file);
       
         if (len < FLASHSIZE) {
           
@@ -112,7 +167,7 @@ void loadRom() {
 
 }
 
-void printRegs() {
+void printRegs(struct CHIP *C) {
 
     register int i;
 
@@ -120,23 +175,23 @@ void printRegs() {
 
     for( i=0; i <= 31; i++ ) 
     {
-       printf("R[%u]:\t%u\t", i, R[i]);
+       printf("R[%u]:\t%x\t", i, C->R[i]);
         
        if( (i+1) % 4 == 0 ) 
        {
-           //printf("%d",i);
            printf("\n");
        }
     }
     printf("\n");
+    printf("SP: %x(%d) PC: %x(%d)\n", C->SP, C->SP, C->PC, C->PC);
 }
 
-void printFlags() {
+void printFlags(struct CHIP *C) {
 
     register int i;
 
     printf("SREG:\n");
-    printf("C:%d Z:%d N:%d V:%d\n", SREG.C, SREG.Z, SREG.N, SREG.V);
-    printf("S:%d H:%d T:%d I:%d\n", SREG.S, SREG.H, SREG.T, SREG.I);
+    printf("C:%x Z:%x N:%x V:%x\n", C->SR.C, C->SR.Z, C->SR.N, C->SR.V);
+    printf("S:%x H:%x T:%x I:%x\n", C->SR.S, C->SR.H, C->SR.T, C->SR.I);
     printf("\n");
 }
